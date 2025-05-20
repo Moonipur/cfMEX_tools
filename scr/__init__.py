8bfda6f0d96cbe5a8ef30596ac4bd69b312b59a4
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-21 00:06:02 +0700

    Update __init__.py

--- scr/__init__.py
+++ scr/__init__.py
@@ -3,7 +3,8 @@ from .cfMEX_EndMotif import *
 #################################################################################################################
 # SETTING
 
-Reference_Genome= '/mnt/sas/ref/hg38/v0/Homo_sapiens_assembly38.fasta'
-EM_window= 'dataset/EM_Window.csv'
+Reference_Genome = '/mnt/sas/ref/hg38/v0/Homo_sapiens_assembly38.fasta'
+Num_1Mb_window = 'dataset/1Mb_Window_Num.csv'
+EM_window = 'dataset/EM_Window.csv'
 
 #################################################################################################################
