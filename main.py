19882a8d9d2d677fc013ef69328e3dcdff4b5253
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-27 10:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
-Code: cfMEX_tools (Version 0.2.0)
+Code: cfMEX_tools (Version 0.3.0)
 By: Songphon Sutthitthasakul (Moon)
-Date: 20-05-2025
+Date: 27-05-2025
 """
 
 from __init__ import *
@@ -21,20 +21,22 @@ def main():
     parser_FS.add_argument("--input", '-i', type=str, help="Fragment file path", required=True)
     parser_FS.add_argument("--output", '-o', type=str, help="Output file name", required=False, default='FS_output')
     parser_FS.add_argument("--id", type=str, help="Sample ID", required=False)
-    parser_FS.add_argument("--thread", '-t', type=int, help="Number of Threads (default: 2)", required=False, default=2)
 
     # End motif
     parser_EM = subparsers.add_parser("endmotif", help="Extract End motif")
     parser_EM.add_argument("--input", '-i', type=str, help="Fragment file path", required=True)
     parser_EM.add_argument("--output", '-o', type=str, help="Output file name", required=False, default='EM_output')
     parser_EM.add_argument("--id", type=str, help="Sample ID", required=False)
-    parser_EM.add_argument("--thread", '-t', type=int, help="Number of Threads (default: 2)", required=False, default=2)
+    parser_EM.add_argument("--thread", '-t', type=int, help="Number of Threads (default: 1)", required=False, default=1)
     
     args = parser.parse_args()
 
     if args.command == "fragsize":
-        pass
+        start_time = time()
 
+        FS_Run(args.input, args.id, args.output)
+        Time_Stamp(start_time)
+        
     elif args.command == "endmotif":
         start_time = time()
         
@@ -58,7 +60,7 @@ def main():
             cfMex_EM().save_csv(args.id, args.output)
             os.remove(f'{args.id}_Metadata.csv')
             Time_Stamp(start_time)
-
+        
 
 if __name__ == "__main__":
     main()
