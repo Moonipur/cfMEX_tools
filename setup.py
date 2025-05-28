17a9f95eed5c4ffefe4105ab9858299f95b99b11
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-28 14:03:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cfMEX_tools',
-    version='0.1',
+    version='0.3.0',
     description='A cell-free DNA multi-feature extraction tools',
     author='Songphon Sutthitthasakul',
     author_email='songphon_sutthittha@cmu.ac.th',
