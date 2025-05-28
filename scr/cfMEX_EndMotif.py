a1a6147af96a2938d911db7e8a3ff226f60b34a0
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-29 00:01:01 +0700

    Update cfMEX_EndMotif.py

--- scr/cfMEX_EndMotif.py
+++ scr/cfMEX_EndMotif.py
@@ -1,7 +1,7 @@
 """
-Code: End Motif Extraction (Version 2.0)
+Code: End Motif Extraction (Version 2.1)
 By: Songphon Sutthitthasakul (Moon)
-Date: 20-05-2025
+Date: 29-05-2025
 """
 
 import __init__ 
@@ -11,25 +11,12 @@ import pandas as pd
 from Bio import SeqIO
 from subprocess import run
 from collections import Counter
+from .Utility import reverse_complement
 
 class cfMex_EM:
     def __init__(self):
         self.reference = SeqIO.to_dict(SeqIO.parse(__init__.Reference_Genome, 'fasta'))
         self.nuc_dict = {}
-    
-    @staticmethod
-    def reverse_complement(seq):
-        comp_dict = {
-        'A': 'T',
-        'T': 'A',
-        'C': 'G',
-        'G': 'C',
-        'N': 'N'
-        }
-        seq_list = list(seq.upper()[::-1])
-        comp_arr = [comp_dict[seq_list[0]],comp_dict[seq_list[1]],comp_dict[seq_list[2]],comp_dict[seq_list[3]]]
-        
-        return ''.join(comp_arr)
     
     def count_reads(self, loc, Input_path, Id):
         nuc_comb = [''.join(p) for p in itertools.product(['A','C','G','T'], repeat=4)]
@@ -41,12 +28,15 @@ class cfMex_EM:
             fields = record.strip().split('\t')
             chrom, start, end, mapq, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), fields[4]
 
-            if strand == '+':
+            if 'chr' not in chrom:
+                chrom = f'chr{chrom}'
+
+            if strand == '+' or strand == '0':
                 record = self.reference[chrom]
                 EM_list.append(str(record.seq[start:start+4]))
-            elif strand == '-':
+            elif strand == '-' or strand == '1':
                 record = self.reference[chrom]
-                EM_list.append(cfMex_EM.reverse_complement(record.seq[end-4:end]))
+                EM_list.append(reverse_complement(record.seq[end-4:end]))
 
         count = Counter(EM_list)
         for nuc in nuc_comb:
@@ -68,3 +58,4 @@ def EM_Run(loc, Input, Id):
     for locate in loc:
         new_EM = cfMex_EM()
         new_EM.count_reads(locate, Input, Id)
+
