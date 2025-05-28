ec998fefcbdc81de7461b36895e5babe3f9da17b
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-28 23:59:55 +0700

    Create cfMEX_Bam2Frag.py

---
+++ scr/cfMEX_Bam2Frag.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+class BAM2FRAG:
+    def __init__(self, input):
+        self.Input = input
+        self.mapq = 30
+        
+    def Convert(self):
+        dummy_chrom = {
+            'chr1': 1, 'chr2': 2,
+            'chr3': 3, 'chr4': 4,
+            'chr5': 5, 'chr6': 6,
+            'chr7': 7, 'chr8': 8,
+            'chr9': 9, 'chr10': 10,
+            'chr11': 11, 'chr12': 12,
+            'chr13': 13, 'chr14': 14,
+            'chr15': 15, 'chr16': 16,
+            'chr15': 15, 'chr16': 16,
+            'chr17': 17, 'chr18': 18,
+            'chr19': 19, 'chr20': 20,
+            'chr21': 21, 'chr22': 22,
+            'chrX': 23, 'chrY': 24,
+        } 
+
+        data = pd.read_csv(
+            self.Input,
+            sep="\t",
+            compression="gzip",
+            header=None,
+            names=["chr", "start", "mapq", "length"]
+        )
+
+        length = data['length'].values.tolist()
+        start = data['start'].values.tolist()
+
+        end = []
+        strand = []
+
+        for idx, i in enumerate(length):
+            end.append(start[idx] + np.abs(length[idx]))
+            if i >= 0:
+                strand.append(0)
+            else:
+                strand.append(1)
+        
+        data['end'] = end
+        data['strand'] = strand
+
+        data = data[['chr', 'start', 'end', 'mapq', 'strand']]
+        data = data.loc[data["mapq"] >= self.mapq]
+        data['chr'] = dummy_chrom[data.iloc[0,0]]
+
+        output = self.Input.replace('.hg38.frag.bgz','.hg38.frag.tsv.bgz')
+        data.to_csv(
+            output, 
+            index=False, 
+            header=False, 
+            sep='\t', 
+            compression='gzip',
+        )
+            
