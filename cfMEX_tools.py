e19985db8cc0ef97b0c19bd3bc6bbc2551a8ef4a
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-05-30 14:46:57 +0700

    Update cfMEX_tools.py

--- cfMEX_tools.py
+++ cfMEX_tools.py
@@ -1,7 +1,7 @@
 """
-Code: cfMEX_tools (Version 0.3.2)
+Code: cfMEX_tools (Version 0.4.0)
 By: Songphon Sutthitthasakul (Moon)
-Date: 29-05-2025
+Date: 30-05-2025
 """
 
 from __init__ import *
@@ -28,6 +28,13 @@ def main():
     parser_EM.add_argument("--output", '-o', type=str, help="Output file name", required=False, default='EM_output')
     parser_EM.add_argument("--id", type=str, help="Sample ID", required=False)
     parser_EM.add_argument("--thread", '-t', type=int, help="Number of Threads (default: 1)", required=False, default=1)
+
+    # SpL Ratio
+    parser_SR = subparsers.add_parser("splratio", help="Extract SpL ratio")
+    parser_SR.add_argument("--input", '-i', type=str, help="Fragment file path", required=True)
+    parser_SR.add_argument("--output", '-o', type=str, help="Output file name", required=False, default='SR_output')
+    parser_SR.add_argument("--id", type=str, help="Sample ID", required=False)
+    parser_SR.add_argument("--thread", '-t', type=int, help="Number of Threads (default: 1)", required=False, default=1)
 
     # BAM-2-Fragment
     parser_BF = subparsers.add_parser("bam2frag", help="Convert BAM to Fragment")
@@ -62,6 +69,27 @@ def main():
             with Pool(args.thread) as p:
                 p.starmap(EM_Run, zip(location, [args.input]*args.thread, [args.id]*args.thread))
             cfMex_EM().save_csv(args.id, args.output)
+            os.remove(f'{args.id}_Metadata.csv')
+            Time_Stamp(start_time)
+
+    elif args.command == "splratio":
+        start_time = time()
+        
+        if args.thread == 0:
+            args.thread = 1
+            
+        if args.thread == 1:
+            location = singleThreads_Location(f"{base_dir}/{Num_1Mb_window}")
+            EM_Run(location, args.input, args.id)
+            cfMex_SR().save_csv(args.id, args.output)
+            os.remove(f'{args.id}_Metadata.csv')
+            Time_Stamp(start_time)
+
+        elif args.thread > 1:
+            location = multiThreads_Location(f"{base_dir}/{Num_1Mb_window}", args.thread)
+            with Pool(args.thread) as p:
+                p.starmap(SR_Run, zip(location, [args.input]*args.thread, [args.id]*args.thread))
+            cfMex_SR().save_csv(args.id, args.output)
             os.remove(f'{args.id}_Metadata.csv')
             Time_Stamp(start_time)
     
