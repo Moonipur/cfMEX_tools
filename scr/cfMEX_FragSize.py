81b32eb15505c92f899de126070cbd1a18203256
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-06 11:31:52 +0700

    Update cfMEX_FragSize.py

--- scr/cfMEX_FragSize.py
+++ scr/cfMEX_FragSize.py
@@ -1,7 +1,7 @@
 """
-Code: Fragment Size Extraction (Version 3.2)
+Code: Fragment Size Extraction (Version 3.3)
 By: Songphon Sutthitthasakul (Moon)
-Date: 30-05-2025
+Date: 06-06-2025
 """
 
 import __init__ 
@@ -11,19 +11,29 @@ import pandas as pd
 from subprocess import run
 
 class cfMex_FS:
-    def __init__(self, Input_path):
+    def __init__(self, Input_path, Format):
         self.max_size = 400
         self.Input_path = Input_path
         self.fragsize = np.zeros(self.max_size)
+        self.format = Format
 
     def count_reads(self, mapq=30):
-        df = pd.read_csv(
-            self.Input_path,
-            sep="\t",
-            compression="gzip",
-            header=None,
-            names=["chr", "start", "end", "mapq", "strand"]
-        )
+        if self.format == 'c5':
+            df = pd.read_csv(
+                self.Input_path,
+                sep="\t",
+                compression="gzip",
+                header=None,
+                names=["chr", "start", "end", "mapq", "strand"]
+            )
+        elif self.format == 'c6':
+            df = pd.read_csv(
+                self.Input_path,
+                sep="\t",
+                compression="gzip",
+                header=None,
+                names=["chr", "start", "end", "mapq", "motif", "strand"]
+            )
         df_clean = df.loc[df["mapq"] >= mapq]
         df_clean["length"] = df_clean["end"] - df_clean["start"]
         df_clean = df_clean.loc[df_clean['length'] <= self.max_size]
@@ -36,7 +46,7 @@ class cfMex_FS:
         with open(f'{Output}.csv', 'a') as outfile:
             run(['echo', f"{Id},{','.join(map(str,self.fragsize.tolist()))}"], stdout=outfile)
 
-def FS_Run(Input, Id, Output):
-    new_FS = cfMex_FS(Input)
+def FS_Run(Input, Id, Output, Format):
+    new_FS = cfMex_FS(Input, Format)
     new_FS.count_reads()
     new_FS.save_csv(Id, Output)
