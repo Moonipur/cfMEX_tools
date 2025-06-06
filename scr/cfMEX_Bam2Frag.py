baf3472f95afbab3cfe35accc0d5d70e92096942
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-06 11:30:42 +0700

    Update cfMEX_Bam2Frag.py

--- scr/cfMEX_Bam2Frag.py
+++ scr/cfMEX_Bam2Frag.py
@@ -1,11 +1,14 @@
 """
-Code: BAM-2-Fragment Convertion (Version 1.3)
+Code: BAM-2-Fragment Convertion (Version 1.4)
 By: Songphon Sutthitthasakul (Moon)
-Date: 29-05-2025
+Date: 06-06-2025
 """
 
+import itertools
 import numpy as np
 import pandas as pd
+from collections import Counter
+from .Utility import reverse_complement
 
 class BAM2FRAG:
     def __init__(self, input):
@@ -34,26 +37,44 @@ class BAM2FRAG:
             sep="\t",
             compression="gzip",
             header=None,
-            names=["chr", "start", "mapq", "length"]
+            names=["chr", "start", "mapq", "length", 'motif']
         )
 
         length = data['length'].values.tolist()
         start = data['start'].values.tolist()
+        motif = data['motif'].values.tolist()
+
+        nuc_comb = [''.join(p) for p in itertools.product(['A','C','G','T'], repeat=4)]
+        nuc_dict = {}
+
+        for nix, nuc in enumerate(nuc_comb):
+            nuc_dict[nuc] = nix + 1
 
         end = []
         strand = []
+        new_motif = []
 
         for idx, i in enumerate(length):
             end.append(start[idx] + np.abs(length[idx]))
             if i >= 0:
                 strand.append(0)
+                try:
+                    new_motif.append(nuc_dict[f'{motif[idx][:4]}'])
+                except:
+                    new_motif.append(0)
             else:
                 strand.append(1)
+                try:
+                    new_motif.append(nuc_dict[f'{reverse_complement(motif[idx][-4:])}'])
+                except:
+                    new_motif.append(0)
+
         
         data['end'] = end
         data['strand'] = strand
+        data['motif'] = new_motif
 
-        data = data[['chr', 'start', 'end', 'mapq', 'strand']]
+        data = data[['chr', 'start', 'end', 'mapq', 'motif', 'strand']]
         data = data.loc[data["mapq"] >= self.mapq]
         data['chr'] = dummy_chrom[data.iloc[0,0]]
 
