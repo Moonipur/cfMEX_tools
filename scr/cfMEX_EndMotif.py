4174c6b29dd3e91e15ccdf0de0809a2349ed7097
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-06 11:31:11 +0700

    Update cfMEX_EndMotif.py

--- scr/cfMEX_EndMotif.py
+++ scr/cfMEX_EndMotif.py
@@ -1,7 +1,7 @@
 """
-Code: End Motif Extraction (Version 2.3)
+Code: End Motif Extraction (Version 2.4)
 By: Songphon Sutthitthasakul (Moon)
-Date: 29-05-2025
+Date: 06-06-2025
 """
 
 import __init__ 
@@ -15,15 +15,18 @@ from collections import Counter
 from .Utility import reverse_complement
 
 class cfMex_EM:
-    def __init__(self):
+    def __init__(self, Format='c5'):
         self.reference = SeqIO.to_dict(SeqIO.parse(__init__.Reference_Genome, 'fasta'))
-        self.nuc_dict = {}
+        self.nuc_dict_ref = {}
+        self.nuc_dict_ori = {}
+        self.format = Format
     
     def count_reads(self, loc, Input_path, Id):
         nuc_comb = [''.join(p) for p in itertools.product(['A','C','G','T'], repeat=4)]
 
         bedfile = pysam.TabixFile(Input_path)
-        EM_list =[]
+        EM_ref =[]
+        EM_ori =[]
 
         with gzip.open(Input_path, 'rt') as file:
             first_line = file.readline()
@@ -32,36 +35,58 @@ class cfMex_EM:
 
         for record in bedfile.fetch(loc[1], loc[2], loc[3]):
             fields = record.strip().split('\t')
-            chrom, start, end, mapq, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), fields[4]
+            if self.format == 'c5':
+                chrom, start, end, mapq, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), fields[4]
+            elif self.format == 'c6':
+                chrom, start, end, mapq, motif, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), int(fields[4]), fields[5]
+                EM_ori.append(motif)
 
             if 'chr' not in chrom:
                 chrom = f'chr{chrom}'
 
             if strand == '+' or strand == '0':
                 record = self.reference[chrom]
-                EM_list.append(str(record.seq[start:start+4]))
+                EM_ref.append(str(record.seq[start:start+4]))
             elif strand == '-' or strand == '1':
                 record = self.reference[chrom]
-                EM_list.append(reverse_complement(record.seq[end-4:end]))
+                EM_ref.append(reverse_complement(record.seq[end-4:end]))
 
-        count = Counter(EM_list)
-        for nuc in nuc_comb:
-            self.nuc_dict[nuc] = count[nuc]
+        count_ref = Counter(EM_ref)
+        if self.format == 'c6':
+            count_ori = Counter(EM_ori)
 
-        data = map(str,list(self.nuc_dict.values()))
-        with open(f'{Id}_Metadata.csv','a') as meta:
-            run(['echo', f'{",".join(data)}'], stdout=meta)
+        for nix, nuc in enumerate(nuc_comb):
+            self.nuc_dict_ref[nuc] = count_ref[nuc]
+            if self.format == 'c6':
+                self.nuc_dict_ori[nuc] = count_ori[nix+1]
+
+        data_ref = map(str,list(self.nuc_dict_ref.values()))
+        if self.format == 'c6':
+            data_ori = map(str,list(self.nuc_dict_ori.values()))
+
+        with open(f'{Id}_Metadata_ref.csv','a') as meta:
+            run(['echo', f'{",".join(data_ref)}'], stdout=meta)
+        
+        if self.format == 'c6':
+            with open(f'{Id}_Metadata_ori.csv','a') as meta:
+                run(['echo', f'{",".join(data_ori)}'], stdout=meta)
 
     @staticmethod
-    def save_csv(Id, Output):
-        data = pd.read_csv(f'{Id}_Metadata.csv', header=None)
+    def save_csv(Id, Output, Format):
+        data = pd.read_csv(f'{Id}_Metadata_ref.csv', header=None)
         sum_data = map(str,data.sum(axis=0).values.tolist())
         with open(f'{Output}.csv','a') as outfile:
             run(['echo', f'{Id},{",".join(sum_data)}'], stdout=outfile)
+        
+        if Format == 'c6':
+            data = pd.read_csv(f'{Id}_Metadata_ori.csv', header=None)
+            sum_data = map(str,data.sum(axis=0).values.tolist())
+            with open(f'{Output}_ori.csv','a') as outfile:
+                run(['echo', f'{Id},{",".join(sum_data)}'], stdout=outfile)
 
 
-def EM_Run(loc, Input, Id):
+def EM_Run(loc, Input, Id, Format):
     for locate in loc:
-        new_EM = cfMex_EM()
+        new_EM = cfMex_EM(Format)
         new_EM.count_reads(locate, Input, Id)
 
