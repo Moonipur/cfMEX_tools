c30a142b98eea1bae11007217afb3cedb3b1568b
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-12 19:46:11 +0700

    Create cfMEX_Range.py

---
+++ scr/cfMEX_Range.py
@@ -0,0 +1,54 @@
+"""
+Code: Each Range of Fragment Size Extraction (Version 1.0)
+By: Songphon Sutthitthasakul (Moon)
+Date: 09-06-2025
+"""
+
+import __init__ 
+import os
+import numpy as np
+import pandas as pd
+from subprocess import run
+
+class cfMex_Range:
+    def __init__(self, Input_path, Format):
+        self.Input_path = Input_path
+        self.format = Format
+
+    def split_fragment(self, loc, mapq=30):
+        if self.format == 'c5':
+            df = pd.read_csv(
+                self.Input_path,
+                sep="\t",
+                compression="gzip",
+                header=None,
+                names=["chr", "start", "end", "mapq", "strand"]
+            )
+        elif self.format == 'c6':
+            df = pd.read_csv(
+                self.Input_path,
+                sep="\t",
+                compression="gzip",
+                header=None,
+                names=["chr", "start", "end", "mapq", "motif", "strand"]
+            )
+        df_clean = df.loc[df["mapq"] >= mapq]
+        df_clean["length"] = df_clean["end"] - df_clean["start"]
+        df_clean = df_clean.loc[df_clean['length'] > loc[0]]
+        df_clean = df_clean.loc[df_clean['length'] <= loc[1]]
+
+        df_clean = df_clean[['chr', 'start', 'end', 'mapq', 'strand']]
+
+        output = self.Input_path.replace('.hg38.bed.gz',f'.{loc[0]}_{loc[1]}.hg38.bed')
+        df_clean.to_csv(
+            output, 
+            index=False, 
+            header=False, 
+            sep='\t',
+            )
+        
+
+def Range_Run(loc, Input, Format):
+    for cfRange in loc:
+        new_FS_EM = cfMex_Range(Input, Format)
+        new_FS_EM.split_fragment(cfRange)
