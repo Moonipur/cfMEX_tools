23e2634aac631173cf6bf055f3e4ae2eda6a3903
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-12 19:45:12 +0700

    Update __init__.py

--- scr/__init__.py
+++ scr/__init__.py
@@ -2,6 +2,7 @@ from .cfMEX_FragSize import *
 from .cfMEX_EndMotif import *
 from .cfMEX_Bam2Frag import *
 from .cfMEX_SplRatio import *
+from .cfMEX_Range import *
 
 #################################################################################################################
 # SETTING
@@ -9,5 +10,6 @@ from .cfMEX_SplRatio import *
 Reference_Genome = '/mnt/sas/ref/hg38/v0/Homo_sapiens_assembly38.fasta'
 Num_1Mb_window = 'dataset/1Mb_Window_Num.csv'
 EM_window = 'dataset/EM_Window.csv'
+FS_range = 'dataset/FS_Range.csv'
 
 #################################################################################################################
