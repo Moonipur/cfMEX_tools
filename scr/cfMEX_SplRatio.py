b86a2c54b0785a1ecf2773f158332c42eda87fec
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-12 19:41:45 +0700

    Update cfMEX_SplRatio.py

--- scr/cfMEX_SplRatio.py
+++ scr/cfMEX_SplRatio.py
@@ -11,8 +11,9 @@ import pandas as pd
 from subprocess import run
 
 class cfMex_SR:
-    def __init__(self):
+    def __init__(self, Format='c5'):
         self.nuc_dict = {}
+        self.format = Format
     
     def count_reads(self, loc, Input_path, Id):
         short = 0
@@ -24,10 +25,13 @@ class cfMex_SR:
             first_line = file.readline()
             if 'chr' not in first_line:
                 loc[1] = loc[1].replace('chr','')
-                
+
         for record in bedfile.fetch(loc[1], loc[2], loc[3]):
             fields = record.strip().split('\t')
-            chrom, start, end, mapq, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), fields[4]
+            if self.format == 'c5':
+                chrom, start, end, mapq, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), fields[4]
+            elif self.format == 'c6':
+                chrom, start, end, mapq, motif, strand = fields[0], int(fields[1]), int(fields[2]), int(fields[3]), int(fields[4]), fields[5]
 
             if 'chr' not in chrom:
                 chrom = f'chr{chrom}'
@@ -56,7 +60,8 @@ class cfMex_SR:
         else:
             print(f'Numbers of Locations is wrong: {data.shape[0]}')
 
-def SR_Run(loc, Input, Id):
+
+def SR_Run(loc, Input, Id, Format):
     for locate in loc:
-        new_SR = cfMex_SR()
+        new_SR = cfMex_SR(Format)
         new_SR.count_reads(locate, Input, Id)
