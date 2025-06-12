0fffdce7b54b256560f0d3228ec8f2be99f4b89e
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-12 19:02:04 +0700

    Update cfMEX_SplRatio.py

--- scr/cfMEX_SplRatio.py
+++ scr/cfMEX_SplRatio.py
@@ -1,7 +1,7 @@
 """
-Code: Short per Long Ratio Extraction (Version 1.2)
+Code: Short per Long Ratio Extraction (Version 1.3)
 By: Songphon Sutthitthasakul (Moon)
-Date: 30-05-2025
+Date: 12-06-2025
 """
 
 import __init__ 
@@ -32,9 +32,9 @@ class cfMex_SR:
             if 'chr' not in chrom:
                 chrom = f'chr{chrom}'
 
-            if end - start >= 151 and end - start <= 250:
+            if end - start >= 151 and end - start <= 250 and mapq >= 30:
                 long += 1
-            elif end - start >= 90 and end - start <= 150:
+            elif end - start >= 90 and end - start <= 150 and mapq >= 30:
                 short += 1
 
         if long == 0:
@@ -49,10 +49,12 @@ class cfMex_SR:
     def save_csv(Id, Output):
         data = pd.read_csv(f'{Id}_Metadata.csv', header=None, names=['index','spl'])
         data = data.sort_values(by=['index'])
-        sum_data = ','.join(map(str,data['spl'].tolist()))
-        with open(f'{Output}.csv','a') as outfile:
-            run(['echo', f'{Id},{sum_data}'], stdout=outfile)
-
+        if data.shape[0] == 2246:
+            sum_data = ','.join(map(str,data['spl'].tolist()))
+            with open(f'{Output}.csv','a') as outfile:
+                run(['echo', f'{Id},{sum_data}'], stdout=outfile)
+        else:
+            print(f'Numbers of Locations is wrong: {data.shape[0]}')
 
 def SR_Run(loc, Input, Id):
     for locate in loc:
