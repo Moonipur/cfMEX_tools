37df0074d84c5bb2892472a20ea6aca39d575131
Moonipur <119776865+Moonipur@users.noreply.github.com>
2025-06-16 16:08:47 +0700

    Update __init__.py

--- scr/__init__.py
+++ scr/__init__.py
@@ -8,8 +8,5 @@ from .cfMEX_SplRatio import *
 # SETTING
 
 Reference_Genome = "/mnt/sas/ref/hg38/v0/Homo_sapiens_assembly38.fasta"
-Num_1Mb_window = "dataset/1Mb_Window_Num.csv"
-EM_window = "dataset/EM_Window.csv"
-FS_range = "dataset/FS_Range.csv"
 
 #################################################################################################################
